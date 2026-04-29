5cda0ed5103ce107537e904accf6a1c37bfa3b3e
Luiz Felipe Moureau <luizfelipesmoureau@gmail.com>
2026-04-29 10:32:26 +0000

    remove SIGHUP from graceful shutdown signals

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -112,7 +112,7 @@ func main() {
 
 	// Listen for syscall signals for graceful shutdown
 	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	go func() {
 		<-sig
 
